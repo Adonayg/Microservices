8369ff4e6e2fcfb1ad86f6f5aa9e960c3f47b9ba
adonayg <adonay@tutanota.com>
2018-12-02 14:35:11 +0000

    First commit

---
+++ Account-number-generator/src/main/java/com/gareth/accountnumbergenerator/AccountNumberGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.gareth.accountnumbergenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountNumberGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountNumberGeneratorApplication.class, args);
+	}
+}
---
+++ Account-prize-checker/src/main/java/com/gareth/accountprizechecker/AccountPrizeCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.gareth.accountprizechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountPrizeCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountPrizeCheckerApplication.class, args);
+	}
+}
